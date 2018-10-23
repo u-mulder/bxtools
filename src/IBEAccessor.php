9bdcd8276ceadc046b9a3c609299b0e605063374
u_mulder <m264695502@gmail.com>
2018-10-23 17:36:44 +0300

    Update `IBEAccessor` - add support for ACTIVE and COUNT modifiers

--- src/IBEAccessor.php
+++ src/IBEAccessor.php
@@ -12,6 +12,11 @@ class IBEAccessor
     protected $navParams = false;
     protected $selectFields;
 
+    /**
+     * IBEAccessor constructor.
+     *
+     * @param $iblock_id
+     */
     public function __construct($iblock_id)
     {
         $this->iblock_id = $iblock_id;
@@ -20,25 +25,24 @@ class IBEAccessor
     /**
      * Подготавливаем аргументы для передачи в `getList`
      *
-     * @param $arguments
-     * @param $modfifiers
+     * @param array $arguments
+     * @param array $modifiers
+     *
      * @return IBEAccessor
      */
     public function prepareArguments(array $arguments, array $modifiers): self
     {
-        // Modificators (active, count)
-
         $this->sort = \is_array($arguments[0]) ? $arguments[0] : [];
 
         $this->filter = \array_merge(
             ['IBLOCK_ID' => $this->iblock_id],
             $arguments[1]
         );
-        /*if (!empty($modifiers['ACTIVE'])) {
+        if (!empty($modifiers[IBEFacade::ACTIVE_PREFIX])) {
             $this->filter['ACTIVE'] = 'Y';
-        }*/
+        }
 
-        if (!empty($modifiers['COUNT'])) {
+        if (!empty($modifiers[IBEFacade::COUNT_SUFFIX])) {
             $this->grouping = [];
         } else {
             $this->grouping = \is_array($arguments[2]) ? $arguments[2] : false;
