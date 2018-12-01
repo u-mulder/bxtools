bd174c19c1a4809394366411535ad031f61329e3
u_mulder <m264695502@gmail.com>
2018-12-01 23:05:51 +0300

    - Improve `SourceFinder` code  - `SourceFinder` code tested on real Bitrix project

--- src/SourceFinder.php
+++ src/SourceFinder.php
@@ -2,14 +2,18 @@
 
 namespace Um\BxTools;
 
+use Bitrix\Iblock\IblockTable;
+use Bitrix\Iblock\IblockSiteTable;
+
 class SourceFinder
 {
     /**
      * Пытаемся найти источник данных по коду.
      *
      * В данном случае это поиск битриксового инфоблока по его коду.
-     * Дополнительно есть возможность передать id сайта,
-     * для которого будет осуществлен поиск инфоблока
+     * Дополнительно есть возможность передать id сайта, которому
+     * принадлежит инфоблок. Если id сайта НЕ передается, но
+     * объявлена константа `SITE_ID`, то будет использована она
      *
      * @param string $code
      * @param string|null $siteId
@@ -19,20 +23,34 @@ class SourceFinder
     {
         $code = trim($code);
         if ($code) {
-            $filter = [
-                '=CODE' => $code,
-            ];
+            $filter = [];
 
             $siteId = null === $siteId ? '' : trim($siteId);
-            if (!$siteId && defined('\SITE_ID')) {
+            if (!$siteId && \defined('\SITE_ID')) {
                 $siteId = \SITE_ID;
             }
-            $siteId && $filter['LID'] = $siteId;
+            $siteId && $filter['SITE_ID'] = $siteId;
+            $noSiteId = empty($filter['SITE_ID']);
 
-            return \Bitrix\Main\IblockTable::getList([
-                'filter' => $filter,
-                'select' => ['ID', 'NAME']
-            ])->fetch();
+            if ($noSiteId) {
+                $filter['=CODE'] = $code;
+
+                $iblock = IblockTable::getList([
+                    'filter' => $filter,
+                    'select' => ['ID', 'NAME'],
+                ])->fetch();
+            } else {
+                $filter['=IBLOCK.CODE'] = $code;
+
+                $iblock = IblockSiteTable::getList([
+                    'filter' => $filter,
+                    'select' => ['IBLOCK.ID', 'IBLOCK.NAME'],
+                ])->fetch();
+            }
+
+            return $iblock
+                ? ['ID' => $iblock[$noSiteId ? 'ID' : 'IBLOCK_IBLOCK_SITE_IBLOCK_ID']]
+                : false;
         }
 
         throw new \UnexpectedValueException('Iblock code is empty');
